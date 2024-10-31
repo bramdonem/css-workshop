45239f40405aada2ddb17a5d534cbbe4bf3c2615
bdecuype <bram.decuypere@rvaonem.fgov.be>
2024-10-31 13:24:38 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +11,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'workshop-css';
+
+  fireworks() {
+    const container = document.querySelector('.container')
+    const fireworks = new (window as any).Fireworks.default(container, { /* options */ })
+    fireworks.start()
+  }
 }
